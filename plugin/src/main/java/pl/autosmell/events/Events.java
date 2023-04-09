eb8e7cad8b7cf900e342e0d117e4252faf70a666
qWojtpl <71585628+qWojtpl@users.noreply.github.com>
2023-04-09 19:26:16 +0200

    Updated drop randomizing
    - Now drops randomizer uses build-in method

--- plugin/src/main/java/pl/autosmell/events/Events.java
+++ plugin/src/main/java/pl/autosmell/events/Events.java
@@ -14,6 +14,7 @@ import org.bukkit.inventory.ItemStack;
 import pl.autosmell.AutoSmell;
 import pl.autosmell.data.DataHandler;
 
+import java.util.Collection;
 import java.util.Random;
 
 public class Events implements Listener {
@@ -24,11 +25,12 @@ public class Events implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onBreak(BlockBreakEvent event) {
         if(event.isCancelled()) return;
-        if(!event.getPlayer().hasPermission(plugin.getPermissionManager().getPermission(dataHandler.getNoCobblestonePermission()))) return;
-        if(!dataHandler.getCobblestoneData().containsKey(event.getPlayer().getName())) return;
-        if(!dataHandler.getCobblestoneData().get(event.getPlayer().getName())) return;
-        ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
-        updateDurability(item, event.getPlayer());
+        Player player = event.getPlayer();
+        if(!player.hasPermission(plugin.getPermissionManager().getPermission(dataHandler.getNoCobblestonePermission()))) return;
+        if(!dataHandler.getCobblestoneData().containsKey(player.getName())) return;
+        if(!dataHandler.getCobblestoneData().get(player.getName())) return;
+        ItemStack item = player.getInventory().getItemInMainHand();
+        updateDurability(item, player);
         for(Material m : dataHandler.getCobblestoneBlocks()) {
             if(event.getBlock().getType().equals(m)) {
                 event.setCancelled(true);
@@ -40,52 +42,47 @@ public class Events implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onBreakOre(BlockBreakEvent event) {
         if(event.isCancelled()) return;
-        if(!event.getPlayer().hasPermission(plugin.getPermissionManager().getPermission(dataHandler.getAutoSmellPermission()))) return;
-        if(event.getPlayer().getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.SILK_TOUCH) > 0) return;
-        if(!dataHandler.getSmellData().containsKey(event.getPlayer().getName())) return;
-        if(!dataHandler.getSmellData().get(event.getPlayer().getName())) return;
+        Player player = event.getPlayer();
+        if(!player.hasPermission(plugin.getPermissionManager().getPermission(dataHandler.getAutoSmellPermission()))) return;
+        ItemStack item = player.getInventory().getItemInMainHand();
+        if(item.getEnchantmentLevel(Enchantment.SILK_TOUCH) > 0) return;
+        if(!dataHandler.getSmellData().containsKey(player.getName())) return;
+        if(!dataHandler.getSmellData().get(player.getName())) return;
+        Collection<ItemStack> drops = event.getBlock().getDrops(item);
+        if(drops.size() != 1) return;
+        int count = 0;
+        for(ItemStack drop : drops) {
+            count = drop.getAmount();
+        }
         Location blockLoc = event.getBlock().getLocation();
         Material blockType = event.getBlock().getType();
-        int fortuneLevel = event.getPlayer().getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS);
-        int fortuneCount = 0;
-        if(fortuneLevel > 0) {
-            int min = 0;
-            Random rand = new Random();
-            fortuneCount = rand.nextInt((fortuneLevel - min) + 1) + min;
-        }
         event.setCancelled(true);
         if(blockType.equals(Material.ANCIENT_DEBRIS)) {
-            if(hasSpace(event.getPlayer(), Material.NETHERITE_SCRAP)) {
-                event.getPlayer().getInventory().addItem(new ItemStack(Material.NETHERITE_SCRAP));
+            if(hasSpace(player, Material.NETHERITE_SCRAP)) {
+                player.getInventory().addItem(new ItemStack(Material.NETHERITE_SCRAP));
             } else {
                 blockLoc.getWorld().dropItem(blockLoc, new ItemStack(Material.NETHERITE_SCRAP));
             }
         } else if(blockType.equals(Material.IRON_ORE) || blockType.equals(Material.DEEPSLATE_IRON_ORE)) {
-            int count = 1 + fortuneCount;
             for (int i = 0; i < count; i++) {
-                if (hasSpace(event.getPlayer(), Material.IRON_INGOT)) {
-                    event.getPlayer().getInventory().addItem(new ItemStack(Material.IRON_INGOT));
+                if (hasSpace(player, Material.IRON_INGOT)) {
+                    player.getInventory().addItem(new ItemStack(Material.IRON_INGOT));
                 } else {
                     blockLoc.getWorld().dropItem(blockLoc, new ItemStack(Material.IRON_INGOT));
                 }
             }
         } else if(blockType.equals(Material.GOLD_ORE) || blockType.equals(Material.DEEPSLATE_GOLD_ORE)) {
-            int count = 1 + fortuneCount;
             for (int i = 0; i < count; i++) {
-                if (hasSpace(event.getPlayer(), Material.GOLD_INGOT)) {
-                    event.getPlayer().getInventory().addItem(new ItemStack(Material.GOLD_INGOT));
+                if (hasSpace(player, Material.GOLD_INGOT)) {
+                    player.getInventory().addItem(new ItemStack(Material.GOLD_INGOT));
                 } else {
                     blockLoc.getWorld().dropItem(blockLoc, new ItemStack(Material.GOLD_INGOT));
                 }
             }
         } else if(blockType.equals(Material.COPPER_ORE) || blockType.equals(Material.DEEPSLATE_COPPER_ORE)) {
-            Random rand = new Random();
-            int min = 2 * (fortuneLevel + 1);
-            int max = 5 * (fortuneLevel + 1);
-            int count = (rand.nextInt((max - min) + 1) + min);
             for (int i = 0; i < count; i++) {
-                if (hasSpace(event.getPlayer(), Material.COPPER_INGOT)) {
-                    event.getPlayer().getInventory().addItem(new ItemStack(Material.COPPER_INGOT));
+                if (hasSpace(player, Material.COPPER_INGOT)) {
+                    player.getInventory().addItem(new ItemStack(Material.COPPER_INGOT));
                 } else {
                     blockLoc.getWorld().dropItem(blockLoc, new ItemStack(Material.COPPER_INGOT));
                 }
@@ -94,8 +91,7 @@ public class Events implements Listener {
             event.setCancelled(false);
         }
         if(event.isCancelled()) {
-            ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
-            updateDurability(item, event.getPlayer());
+            updateDurability(item, player);
             event.getBlock().setType(Material.AIR);
         }
     }
@@ -119,28 +115,27 @@ public class Events implements Listener {
     }
 
     public void updateDurability(ItemStack item, Player player) {
-        if(!item.getType().equals(Material.AIR)) {
-            int max = item.getType().getMaxDurability();
-            if(max == 0) return;
-            if(!item.getItemMeta().isUnbreakable()) {
-                int enchantmentLevel = item.getItemMeta().getEnchantLevel(Enchantment.DURABILITY);
-                if(enchantmentLevel > 0) {
-                    int chance = 100 / (enchantmentLevel + 1);
-                    int random = (int) Math.floor(Math.random() * (100 + 1));
-                    if (random < chance) {
-                        item.setDurability((short) (item.getDurability() + 1));
-                        if(item.getDurability() >= max) {
-                            player.getInventory().setItem(player.getPlayer().getInventory().getHeldItemSlot(), new ItemStack(Material.AIR));
-                            player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0F, 1.0F);
-                        }
-                    }
-                } else {
-                    item.setDurability((short) (item.getDurability() + 1));
-                    if(item.getDurability() >= max) {
-                        player.getInventory().setItem(player.getPlayer().getInventory().getHeldItemSlot(), new ItemStack(Material.AIR));
-                        player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0F, 1.0F);
-                    }
+        if(item == null) return;
+        if(item.getType().equals(Material.AIR)) return;
+        int max = item.getType().getMaxDurability();
+        if(max == 0) return;
+        if(item.getItemMeta().isUnbreakable()) return;
+        int enchantmentLevel = item.getItemMeta().getEnchantLevel(Enchantment.DURABILITY);
+        if(enchantmentLevel > 0) {
+            int chance = 100 / (enchantmentLevel + 1);
+            int random = (int) Math.floor(Math.random() * (100 + 1));
+            if (random < chance) {
+                item.setDurability((short) (item.getDurability() + 1));
+                if(item.getDurability() >= max) {
+                    player.getInventory().setItem(player.getInventory().getHeldItemSlot(), new ItemStack(Material.AIR));
+                    player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0F, 1.0F);
                 }
+            }
+        } else {
+            item.setDurability((short) (item.getDurability() + 1));
+            if(item.getDurability() >= max) {
+                player.getInventory().setItem(player.getInventory().getHeldItemSlot(), new ItemStack(Material.AIR));
+                player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0F, 1.0F);
             }
         }
     }
